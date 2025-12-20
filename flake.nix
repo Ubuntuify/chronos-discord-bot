ada42a1bbf41adfeeb54efcb50e9d5f26a097736
Ryan Salazar <ryanconrad2007@gmail.com>
2025-12-21 00:31:41 +0800

    chore(flake): adds dev shell for x86_64 hosts

--- flake.nix
+++ flake.nix
@@ -6,18 +6,34 @@
   outputs = {
     self,
     nixpkgs,
-  }: let
-    pkgs = nixpkgs.legacyPackages."aarch64-linux";
-  in {
-    devShells."aarch64-linux".default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        cargo
-        rustc
-        rustfmt
-        clippy
-        rust-analyzer
-      ];
-      env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  }: {
+    devShells = {
+      "aarch64-linux".default = let
+        pkgs = nixpkgs.legacyPackages."aarch64-linux";
+      in
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+            clippy
+            rust-analyzer
+          ];
+          env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
+      "x86_64-linux".default = let
+        pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      in
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            rustc
+            rustfmt
+            clippy
+            rust-analyzer
+          ];
+          env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
     };
   };
 }
