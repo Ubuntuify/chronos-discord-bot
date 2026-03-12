a12a16b1cddaf667b7844146de65259b46ec1c2d
Ryan Salazar <ryanconrad2007@gmail.com>
2026-03-12 10:01:46 +0800

    flake: Update flake to use flake-utils to remove redundancy.

--- flake.nix
+++ flake.nix
@@ -1,17 +1,20 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = {
     self,
     nixpkgs,
-  }: {
-    devShells = {
-      "aarch64-linux".default = let
-        pkgs = nixpkgs.legacyPackages."aarch64-linux";
-      in
-        pkgs.mkShell {
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             cargo
             rustc
@@ -21,19 +24,6 @@
           ];
           env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
         };
-      "x86_64-linux".default = let
-        pkgs = nixpkgs.legacyPackages."x86_64-linux";
-      in
-        pkgs.mkShell {
-          buildInputs = with pkgs; [
-            cargo
-            rustc
-            rustfmt
-            clippy
-            rust-analyzer
-          ];
-          env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-        };
-    };
-  };
+      }
+    );
 }
